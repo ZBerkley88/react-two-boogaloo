2d933297107ecc3db2a98468655890dc9e503389
Zack <zachary.berkley@gmail.com>
2023-03-21 22:37:51 -0400

    added details to about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,42 @@
- import React from 'react'
- import './about.css'
- 
- const About = () => {
-   return (
-     <section id="about">
-       About
-     </section>
-   )
- }
- 
- export default About
- 
+import React from "react";
+import "./about.css";
+//  import ME from '../../assets/about-me.jpg'
+
+import { FaUserGraduate } from "react-icons/fa"
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            {/* <img src={ME} alt="About image" /> */}
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaUserGraduate className="about__icon"/>
+              <h5>Experience</h5>
+              <small>7+ years teaching</small>
+            </article>
+            <article className="about__card">
+              <FaUserGraduate className="about__icon"/>
+              <h5>Experience</h5>
+              <small>7+ years teaching</small>
+            </article>
+            <article className="about__card">
+              <FaUserGraduate className="about__icon"/>
+              <h5>Experience</h5>
+              <small>7+ years teaching</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
